5b6b5cacc7163a04b867fa24c96f24f880571bdf
IKENNA <ikennavictorofficial@gmail.com>
2021-12-05 00:01:53 +0100

    maps

--- src/App.js
+++ src/App.js
@@ -3,46 +3,39 @@ import './App.css';
 import Counter from './Counter';
 import Employee from './Employee';
 
-const employee = {
+const employees = [
+  {
     id: '1',
     firstName: 'Ikenna',
     lastName: 'Odoh',
-    age: '20',
-    contact: {
-      phone: '+2348164114571',
-      email: 'ikenna@tech.com',
-      address: {
-        street: '66 Chime Avenue',
-        city: 'Enugwu',
-        country: {
-          id: '2',
-          name: 'Nigeria',
-          alias: 'NGR'
-        }
-      }
-    }
-  }
+    age: '20'
+  },
+  {
+    id: '2',
+    firstName: 'UG',
+    lastName: 'Odoh',
+    age: '18'
+  },
+  {
+    id: '3',
+    firstName: 'TC',
+    lastName: 'Odoh',
+    age: '16'
+  },
+]
 
 function App() {
-  const {
-    id: userId,
-    firstName,
-    lastName,
-    age,
-    contact: {phone, email, address: {
-      street, city, country: {id: countryId, name, alias}
-    }} 
-   } = employee;
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Employee Detail</h1>
-        <h4>SN: {userId}</h4>
-        <h4>Name: {firstName} {lastName}</h4>
-        <h4>Age: {age}</h4>
-        <h4>Contact: {phone} ({alias})</h4>
-        <h4>Email: {email}</h4>
-        <h4>Address: {street}, {city}, {name} ({countryId})</h4>
+      {
+        employees.map((employee) => {
+          const {id, firstName, lastName, age} = employee
+          return(
+            <Employee key={id} id={id} firstName={firstName} lastName={lastName} age={age} />
+          )
+        })
+      }
       </header>
     </div>
   );
