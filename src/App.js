c465adf5b3beccc7cb5e29d16e16a921e25c19c9
IKENNA <ikennavictorofficial@gmail.com>
2021-12-04 19:41:57 +0100

    Destructuring

--- src/App.js
+++ src/App.js
@@ -3,35 +3,45 @@ import './App.css';
 import Counter from './Counter';
 import Employee from './Employee';
 
-const employees = [
-  {
+const employee = {
+    id: '1',
     firstName: 'Ikenna',
     lastName: 'Odoh',
-    age: '20'
-  },
-  {
-    firstName: 'Ifeanyi',
-    lastName: 'Okafo',
-    age: '18'
-  },
-  {
-    firstName: 'Abel',
-    lastName: 'Ani',
-    age: '16'
+    age: '20',
+    contact: {
+      phone: '+2348164114571',
+      email: 'ikenna@tech.com',
+      address: {
+        street: '66 Chime Avenue',
+        city: 'Enugwu',
+        country: {
+          id: '2',
+          name: 'Nigeria',
+          alias: 'NGR'
+        }
+      }
+    }
   }
-]
 
 function App() {
+  const {
+    id: userId,
+    firstName,
+    lastName,
+    age,
+    contact: {phone, email, address: {
+      street, city, country: {id: countryId, name, alias}
+    }} 
+   } = employee;
   return (
     <div className="App">
       <header className="App-header">
-        {
-          employees.map((employee) => {
-            return (
-              <Employee key={employee.age} {...employee}/>
-            );
-          })
-        } 
+        <h1>Employee Detail</h1>
+        <h4>SN: {userId}</h4>
+        <h4>Name: {firstName} {lastName}</h4>
+        <h4>Age: {age}</h4>
+        <h4>Contact: {phone} ({alias})</h4>
+        <h4>Address: {street}, {city}, {name} ({countryId})</h4>
       </header>
     </div>
   );
