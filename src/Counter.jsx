abb3af326b258d5568f01f4ed634d3b62408fd5b
IKENNA <ikennavictorofficial@gmail.com>
2021-12-09 09:27:26 +0100

    redux ducks

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,25 @@
 import React, {useState} from 'react'
+import { useSelector } from 'react-redux';
 
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+    const count = useSelector((state) => state.counter.count);
+
+    const increment = () => {}
+
+    const decrement = () => {}
 
     return (
         <React.Fragment>
-            <p>The count is: {count} </p>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decreament</button>
+            <div style={{ backgroundColor: 'gray', margin: '10px' }}>
+                <h3>The count is: {count} </h3>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <button onClick={increment}>Increment</button>
+                    <button onClick={decrement}>Decrement</button>
+                </div>
+                
+            </div>
+            
         </React.Fragment>
     );
 }
